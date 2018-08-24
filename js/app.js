8f42943b9be5a14e34443c9e440cfd25e27f54ae
lisaaoyama <lisaaoyama85@gmail.com>
2018-08-24 13:08:21 +0200

    modified mobile menu

--- js/app.js
+++ js/app.js
@@ -19,14 +19,14 @@ $(function() {
 
   $(".cross, .mobile-menu").hide();
   $( ".hamburger" ).click(function() {
-    $( ".mobile-menu" ).slideToggle( "fast", function() {
+    $( ".mobile-menu" ).animate({width: 'toggle'}, 300, function() {
     $( ".hamburger" ).hide();
     $( ".cross" ).show();
     });
   });
 
   $( ".cross" ).click(function() {
-    $( ".mobile-menu" ).slideToggle( "fast", function() {
+    $( ".mobile-menu" ).animate({width: 'toggle'}, 300, function() {
     $( ".cross" ).hide();
     $( ".hamburger" ).show();
     });
