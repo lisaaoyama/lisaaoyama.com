b6602e3942349764ee4e1cef22560ccaeb9ddd07
lisaaoyama <lisaaoyama85@gmail.com>
2018-09-01 22:14:26 +0200

    fixed mobile menu, changed fade in for works page from js to css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 $(function() {
-  $(".work-container").hover(
-    function() {
-      // $(this).find(".work-title-container").fadeOut(0.1);
-      $(this).find(".work-descr-container").fadeIn();
-      $(this).children(".work-container-overlay").css("background-color", "rgba(255,255,255,0.7)");
-    },
-    function() {
-      // $(this).find(".work-title-container").fadeIn();
-      $(this).find(".work-descr-container").fadeOut(0.1);
-      $(this).children(".work-container-overlay").css("background-color", "rgba(255,255,255,0.1)");
-    }
-  )
+  // $(".work-container").hover(
+  //   function() {
+  //     // $(this).find(".work-title-container").fadeOut(0.1);
+  //     $(this).find(".work-descr-container").fadeIn();
+  //     $(this).children(".work-container-overlay").css("background-color", "rgba(255,255,255,0.7)");
+  //   },
+  //   function() {
+  //     // $(this).find(".work-title-container").fadeIn();
+  //     $(this).find(".work-descr-container").fadeOut(0.1);
+  //     $(this).children(".work-container-overlay").css("background-color", "rgba(255,255,255,0.1)");
+  //   }
+  // )
 
 
   $("#page").removeClass("fade-out");
